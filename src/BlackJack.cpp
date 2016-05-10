3295106c2dcaabb5f8c2ab98f0bbbeca19330ec8
whitesadd <david.blancop@gmail.com>
2016-05-10 19:39:51 +0200

    Fix indentation in main()
    Change-Id: I83eaa1a7e4cd57f72115efc75eac9f377917549d

--- src/BlackJack.cpp
+++ src/BlackJack.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
 
     cout << endl << "!!!Welcome to Black Jack TDD!!!" << endl << endl;
     if (RUN_ALL_TESTS() !=0) {
